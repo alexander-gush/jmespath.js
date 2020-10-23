ba58f37a3f0ce2c4b0d301e9fef8e22763db9fba
alexander-gush <39790338+alexander-gush@users.noreply.github.com>
2020-10-23 11:39:05 +0300

    Update jmespath.js

--- jmespath.js
+++ jmespath.js
@@ -1322,7 +1322,7 @@
     },
 
     _functionStartsWith: function(resolvedArgs) {
-        return resolvedArgs[0].lastIndexOf(resolvedArgs[1]) === 0;
+        return resolvedArgs[0].startsWith(resolvedArgs[1]);
     },
 
     _functionEndsWith: function(resolvedArgs) {
